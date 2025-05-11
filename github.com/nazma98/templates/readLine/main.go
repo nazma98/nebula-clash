effd07b074a90430d9e002106319e4359d1490bf
nazma98 <nazmasarker1311@gmail.com>
2025-05-11 23:16:44 +0600

    Add templates for fast i/o

---
+++ github.com/nazma98/templates/readLine/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	line, _ := reader.ReadString('\n')
+	parts := strings.Fields(line)
+
+	a, _ := strconv.Atoi(parts[0])
+	fmt.Println(a)
+}
---
+++ github.com/nazma98/templates/readTestcaseLine/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	t, _ := strconv.Atoi(scanner.Text())
+
+	for i := 0; i < t; i++ {
+		scanner.Scan()
+		n, _ := strconv.Atoi(scanner.Text())
+		fmt.Println(n)
+	}
+}
