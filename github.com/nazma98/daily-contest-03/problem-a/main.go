3a13287e7bc4162162e2780f186f9f2cd444bbfd
nazma98 <nazmasarker1311@gmail.com>
2025-05-13 20:54:19 +0600

    Add daily ocntest 3 problem a

---
+++ github.com/nazma98/daily-contest-03/problem-a/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	t, _ := strconv.Atoi(scanner.Text())
+
+	for i := 0; i < t; i++ {
+		scanner.Scan()
+		n, _ := strconv.Atoi(scanner.Text())
+	}
+}
