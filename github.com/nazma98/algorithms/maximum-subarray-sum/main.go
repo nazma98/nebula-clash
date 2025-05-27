9f3ec21254705fa0f149eb0afc7555af1f3b7ddf
nazma98 <nazmasarker1311@gmail.com>
2025-05-27 18:42:20 +0600

    Print the subarray using kadane's algortihm

--- github.com/nazma98/algorithms/maximum-subarray-sum/main.go
+++ github.com/nazma98/algorithms/maximum-subarray-sum/main.go
@@ -8,18 +8,25 @@ func main() {
 	maxSum := -9999999
 	currentSum := 0
 
-	for _, val := range slice {
+	startIndex := 0
+	endIndex := 0
+	tempStart := 0
+
+	for i, val := range slice {
 		currentSum += val
+
 		if maxSum < currentSum {
 			maxSum = currentSum
+			startIndex = tempStart
+			endIndex = i
 		}
 
 		if currentSum < 0 {
 			currentSum = 0
+			tempStart = i + 1
 		}
 	}
 
 	fmt.Println(maxSum)
-
-	fmt.Println(slice)
+	fmt.Println(slice[startIndex : endIndex+1])
 }
